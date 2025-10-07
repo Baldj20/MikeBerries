1427240d3a579744f27e64b4ecdf6e37ce5bb085
Mike <mihal275@mail.ru>
2025-10-07 17:41:03 +0300

    refactor: add title attribute to product table

--- ProductService/ProductService/ProductService.DAL/Entities/Product.cs
+++ ProductService/ProductService/ProductService.DAL/Entities/Product.cs
@@ -2,6 +2,7 @@
 
 public class Product : BaseEntity
 {
+    public string Title { get; set; }
     public string Description { get; set; }
     public decimal Price { get; set; }
     public Guid ProviderId { get; set; }
