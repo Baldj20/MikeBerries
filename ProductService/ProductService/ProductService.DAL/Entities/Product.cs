7e715a6abbd13ab60bca339168f9c2063d09f9be
Mike <mihal275@mail.ru>
2025-10-07 14:36:23 +0300

    refactor: move navigation properties to the bottom of the entity

--- ProductService/ProductService/ProductService.DAL/Entities/Product.cs
+++ ProductService/ProductService/ProductService.DAL/Entities/Product.cs
@@ -4,8 +4,8 @@ public class Product
 {
     public string Article { get; set; }
     public string Description { get; set; }
-    public ICollection<ProductImage> Images { get; set; }
-    public Provider Provider { get; set; }
     public string ProviderEmail { get; set; }
     public int Price { get; set; }
+    public ICollection<ProductImage> Images { get; set; }
+    public Provider Provider { get; set; }
 }
--- ProductService/ProductService/ProductService.DAL/Entities/ProductImage.cs
+++ ProductService/ProductService/ProductService.DAL/Entities/ProductImage.cs
@@ -3,7 +3,7 @@
 public class ProductImage
 {
     public int Id { get; set; }
-    public Product Product { get; set; }
     public string ProductArticle { get; set; }
     public string Url { get; set; }
+    public Product Product { get; set; }
 }
