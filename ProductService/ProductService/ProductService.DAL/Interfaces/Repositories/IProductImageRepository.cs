70ff8bd33abc1e5aa87ce4f2419a8e2d3b449fad
Mike <mihal275@mail.ru>
2025-10-07 14:50:56 +0300

    refactor: add CancellationToken

--- ProductService/ProductService/ProductService.DAL/Interfaces/Repositories/IProductImageRepository.cs
+++ ProductService/ProductService/ProductService.DAL/Interfaces/Repositories/IProductImageRepository.cs
@@ -4,5 +4,5 @@ namespace ProductService.DAL.Interfaces.Repositories;
 
 public interface IProductImageRepository : IRepository<ProductImage>
 {
-    public Task<ICollection<ProductImage>> GetAllProductImagesByArticle(string article);
+    public Task<ICollection<ProductImage>> GetAllProductImagesByArticle(string article, CancellationToken token);
 }
--- ProductService/ProductService/ProductService.DAL/Repositories/ProductImageRepository.cs
+++ ProductService/ProductService/ProductService.DAL/Repositories/ProductImageRepository.cs
@@ -6,9 +6,9 @@ namespace ProductService.DAL.Repositories;
 
 internal class ProductImageRepository(MikeBerriesDBContext context) : Repository<ProductImage>(context), IProductImageRepository
 {
-    public async Task<ICollection<ProductImage>> GetAllProductImagesByArticle(string article)
+    public async Task<ICollection<ProductImage>> GetAllProductImagesByArticle(string article, CancellationToken token)
     {
-        var images = await context.ProductImages.Where(i => i.ProductArticle == article).ToListAsync();
+        var images = await context.ProductImages.Where(i => i.ProductArticle == article).ToListAsync(token);
        
         return images;
     }
