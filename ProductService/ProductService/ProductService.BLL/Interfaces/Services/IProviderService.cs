57a23d01d2ce7178d970e71d99c34141a356b461
Baldj <mihal275@mail.ru>
2025-10-13 22:23:46 +0300

    feat: implement provider service

---
+++ ProductService/ProductService/ProductService.BLL/Interfaces/Services/IProviderService.cs
@@ -0,0 +1,15 @@
+﻿using ProductService.BLL.Models;
+using ProductService.DAL;
+using ProductService.DAL.Filters;
+
+namespace ProductService.BLL.Interfaces.Services;
+
+public interface IProviderService
+{
+    public Task<Result> AddProviderAsync(ProviderModel providerModel, CancellationToken token);
+    public Task<Result> DeleteProviderAsync(Guid id, CancellationToken token);
+    public Task<Result<ProviderModel>> GetProviderByIdAsync(Guid id, CancellationToken token);
+    public Task<Result<List<ProviderModel>>> GetProvidersAsync(PaginationParams paginationParams,
+        ProviderFilter filter, CancellationToken token);
+    public Task<Result> UpdateAsync(Guid id, ProviderModel providerModel, CancellationToken token);
+}
