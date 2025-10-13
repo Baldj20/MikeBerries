ad0aad5bfac727adb64bc63ca321c64fe7828e3b
Baldj <mihal275@mail.ru>
2025-10-13 22:53:47 +0300

    feat: implement product controller

---
+++ ProductService/ProductService/ProductService.API/Controllers/ProductController.cs
@@ -0,0 +1,57 @@
+﻿using Mapster;
+using Microsoft.AspNetCore.Mvc;
+using ProductService.BLL.DTO;
+using ProductService.BLL.Interfaces.Services;
+using ProductService.BLL.Models;
+using ProductService.DAL;
+using ProductService.DAL.Filters;
+
+namespace ProductService.API.Controllers;
+
+[Route("api/products")]
+[ApiController]
+public class ProductController(IProductService productService) : ControllerBase
+{
+    [HttpPost]
+    public async Task<ActionResult> Add(CreateProductDTO dto, CancellationToken token)
+    {
+        var response = await productService.AddProductAsync(dto.Adapt<ProductModel>(), token);
+
+        return response.IsSuccess ? Ok(response) : BadRequest(response.Error);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> Delete(Guid id, CancellationToken token)
+    {
+        var response = await productService.DeleteProductAsync(id, token);
+
+        return response.IsSuccess ? Ok(response) : BadRequest(response.Error);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult> GetById(Guid id, CancellationToken token)
+    {
+        var response = await productService.GetProductByIdAsync(id, token);
+
+        return response.IsSuccess ? Ok(response.Value) : BadRequest(response.Error);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> GetAllPaged(
+        [FromQuery] PaginationParams paginationParams,
+        [FromQuery] ProductFilter filter, 
+        CancellationToken token)
+    {
+        var response = await productService.GetProductsAsync(paginationParams, filter, token);
+
+        return response.IsSuccess ? Ok(response.Value) : BadRequest(response.Error);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> Update(Guid id, UpdateProductDTO dto, CancellationToken token)
+    {
+        var response = await productService.UpdateAsync(id, dto.Adapt<ProductModel>(), token);
+
+        return response.IsSuccess ? Ok(response) : BadRequest(response.Error);
+    }
+}
--- ProductService/ProductService/ProductService.API/DTO/UpdateProductDTO.cs
+++ ProductService/ProductService/ProductService.API/DTO/UpdateProductDTO.cs
@@ -2,7 +2,6 @@
 
 public class UpdateProductDTO
 {
-    public Guid ProductId { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
     public decimal Price { get; set; }
