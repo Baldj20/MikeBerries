b492458336092ef4aeec535baa39539b0ea0e8d4
Mike <mihal275@mail.ru>
2025-10-16 16:00:04 +0300

    refactor: remove redundant mapping configuration

--- ProductService/ProductService/ProductService.BLL/Configurations/BusinessLogicLayerExtension.cs
+++ ProductService/ProductService/ProductService.BLL/Configurations/BusinessLogicLayerExtension.cs
@@ -1,9 +1,6 @@
-﻿using Mapster;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using ProductService.BLL.Interfaces.Services;
-using ProductService.BLL.Models;
 using ProductService.BLL.Services;
-using ProductService.DAL.Entities;
 
 namespace ProductService.BLL.Configurations;
 
@@ -13,12 +10,5 @@ public static class BusinessLogicLayerExtension
     {
         services.AddScoped<IProductService, Services.ProductService>();
         services.AddScoped<IProviderService, ProviderService>();
-    }
-
-    public static void ConfigureMapping()
-    {
-        TypeAdapterConfig<ProviderModel, Provider>.NewConfig().TwoWays();
-        TypeAdapterConfig<ProductModel, Product>.NewConfig().TwoWays();
-        TypeAdapterConfig<ProductImageModel, ProductImage>.NewConfig().TwoWays();
     }
 }
