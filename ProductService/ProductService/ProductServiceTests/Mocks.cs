fb1c979ba871f3ff159de162e9e23d3ce6df08b1
Mike <mihal275@mail.ru>
2025-10-24 18:50:36 +0300

    add test base

---
+++ ProductService/ProductService/ProductServiceTests/Mocks.cs
@@ -0,0 +1,30 @@
+﻿using AutoFixture;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using NSubstitute;
+using ProductService.BLL.Interfaces.Services;
+using ProductService.DAL;
+using ProductService.DAL.Entities;
+using ProductService.DAL.Interfaces.Repositories;
+
+namespace UnitTests;
+
+public class Mocks
+{
+    protected readonly IFixture _fixture;
+
+    protected readonly ILogger<ProductService.BLL.Services.ProductService> _logger;
+    protected readonly IRepository<Product> _productRepository;
+    protected readonly IUnitOfWork _unitOfWork;
+    protected readonly MikeBerriesDBContext _dbContext;
+    protected readonly IProductService _productService;
+    protected Mocks()
+    {
+        _fixture = new Fixture();
+        _productRepository = Substitute.For<IRepository<Product>>();
+        _logger = Substitute.For<ILogger<ProductService.BLL.Services.ProductService>>();
+        _dbContext = Substitute.For<MikeBerriesDBContext>();
+        _unitOfWork = Substitute.For<IUnitOfWork>();
+        _productService = new ProductService.BLL.Services.ProductService(_unitOfWork, _logger);
+    }
+}
---
+++ ProductService/ProductService/ProductServiceTests/ProductServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using AutoFixture;
+using ProductService.BLL.Models;
+using Xunit;
+
+namespace UnitTests;
+
+public class ProductServiceTests : Mocks
+{
+    [Fact]
+    public async Task AddProductAsync_WhenDataIsValid_ShouldReturnSuccessResult()
+    {
+        //Arrange
+        var productModel = _fixture.Create<ProductModel>();
+
+        var response = await _productService.AddProductAsync(productModel, default);
+
+        Assert.True(response.IsSuccess);
+    }
+}
