7e031ab409f62c0c9d930553c7643a1d8676d10d
Mike <mihal275@mail.ru>
2025-11-20 14:49:13 +0300

    refactor(integration_tests): change descriptors removing and add fake file repository

--- ProductService/ProductService.IntegrationTests/ProductServiceWebApplicationFactory.cs
+++ ProductService/ProductService.IntegrationTests/ProductServiceWebApplicationFactory.cs
@@ -2,11 +2,12 @@
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using ProductService.API;
 using ProductService.DAL;
+using ProductService.DAL.Interfaces.Repositories;
 
 namespace ProductService.IntegrationTests;
 
@@ -15,31 +16,25 @@ public class ProductServiceWebApplicationFactory : WebApplicationFactory<Program
     private readonly InMemoryDatabaseRoot _root = new();
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
-        builder.ConfigureAppConfiguration((context, config) =>
-        {
-            var testPath = AppContext.BaseDirectory;
-            var configPath = Path.Combine(testPath, "appsettings.IntegrationTests.json");
-            config.Sources.Clear();
-            config.AddJsonFile(configPath, optional: false, reloadOnChange: false);
-        });
-
         builder.UseEnvironment("IntegrationTesting");
 
         builder.ConfigureServices(services =>
         {
-            var descriptorsToRemove = services.Where(d =>
-                d.ServiceType.Name.Contains("DbContextOptions")           
-            ).ToList();
+            services.RemoveAll<DbContextOptions<MikeBerriesDBContext>>();
+            services.RemoveAll<MikeBerriesDBContext>();
 
-            foreach (var descriptor in descriptorsToRemove)
-            {
-                services.Remove(descriptor);
-            }
+            services.RemoveAll<IFileRepository>();
+            services.RemoveAll<MinioStorage>();
 
             services.AddDbContext<MikeBerriesDBContext>(options =>
             {
                 options.UseInMemoryDatabase("TestDatabase", _root);
-            });          
+            });
+
+            services.AddScoped<IFileRepository>(provider =>
+            {
+                return new FakeFileRepository();
+            });
         });
     }
 }
