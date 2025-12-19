7081c5c747869b37d92f5dad8680b7ad75fbb038
Mike <mihal275@mail.ru>
2025-12-19 11:14:52 +0300

    feat: add entities and configurations

---
+++ UserService/UserService.DAL/Entities/Cart.cs
@@ -0,0 +1,11 @@
+﻿namespace UserService.DAL.Entities;
+
+public class Cart
+{
+    public required string UserId { get; set; }
+    public required List<CartItem> Items { get; set; } = new();
+    public required int TotalPrice { get; set; }
+    public required int TotalCount { get; set; }
+
+    public required User User { get; set; }
+}
---
+++ UserService/UserService.DAL/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace UserService.DAL.Entities;
+
+public class User
+{
+    public required string Auth0Id { get; set; }
+    public required string Email { get; set; }
+    public required string Name { get; set; }
+
+    public required Cart Cart { get; set; }
+}
