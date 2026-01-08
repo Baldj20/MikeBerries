f9ad44af0387dcc654339e0dcb61917a9b6e3845
Mike <mihal275@mail.ru>
2026-01-08 15:46:55 +0300

    fix: fix app perfomance

--- ProductService/ProductService.BLL/Configurations/BusinessLogicLayerExtension.cs
+++ ProductService/ProductService.BLL/Configurations/BusinessLogicLayerExtension.cs
@@ -1,16 +1,13 @@
-﻿using Mapster;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ProductService.BLL.Interfaces.Services;
-using ProductService.BLL.Models;
 using ProductService.BLL.Services;
-using ProductService.DAL.Entities;
-using Serilog;
 
 namespace ProductService.BLL.Configurations;
 
 public static class BusinessLogicLayerExtension
 {
-    public static void ConfigureBusinessLogicLayerDependencies(this IServiceCollection services)
+    public static void ConfigureBusinessLogicLayerDependencies(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddScoped<IProductService, Services.ProductService>();
         services.AddScoped<IProviderService, ProviderService>();
