4f5229465cdfbb091263564a0916b3b74a7b718f
Mike <mihal275@mail.ru>
2026-01-08 17:13:47 +0300

    feat: add products page base according to fsd part 2

---
+++ FrontEnd/mikeberries-frontend/src/app/App.tsx
@@ -0,0 +1,12 @@
+import ProductsPage from "../pages/products/ui/ProductsPage.tsx";
+
+
+function App() {
+  return (
+    <div style={{backgroundColor: "red", width: "100%", minHeight: "100vh"}}>
+        <ProductsPage />
+    </div>
+  )
+}
+
+export default App
