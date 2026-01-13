bf1093f570e46824e2172106f2e3d5fc321910f0
Mike <158204587+Baldj20@users.noreply.github.com>
2026-01-13 15:02:42 +0300

    add repositories
    * feat: add repositories
    
    * Update UserServiceCI.yml

---
+++ UserService/UserService.DAL/Repositories/CartRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserService.DAL.Entities;
+using UserService.DAL.Repositories.Interfaces;
+
+namespace UserService.DAL.Repositories;
+
+public class CartRepository(UserServiceDbContext context) : Repository<Cart>(context), ICartRepository
+{
+    public async Task<Cart?> GetCartByUserId(string userId)
+    {
+        var cart = await Context.Carts
+            .Where(c => c.UserId == userId)
+            .Include(c => c.Items)
+            .FirstOrDefaultAsync();
+
+        return cart;
+    }
+}
---
+++ UserService/UserService.DAL/Repositories/Interfaces/ICartRepository.cs
@@ -0,0 +1,8 @@
+﻿using UserService.DAL.Entities;
+
+namespace UserService.DAL.Repositories.Interfaces;
+
+public interface ICartRepository
+{
+    Task<Cart?> GetCartByUserId(string  userId);
+}
