70f8f52cbc75535da74ce4c43a729f10e2903c6b
Mike <mihal275@mail.ru>
2026-01-20 15:32:52 +0300

    refactor: merge items and carts contollers

--- UserService/UserService.API/Controllers/CartController.cs
+++ UserService/UserService.API/Controllers/CartController.cs
@@ -2,6 +2,8 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using UserService.API.DTOs;
+using UserService.BLL.Carts.Commands.AddItemToCart;
+using UserService.BLL.Carts.Commands.DeleteItemFromCart;
 using UserService.BLL.Carts.Queries.GetUserCart;
 
 namespace UserService.API.Controllers;
@@ -21,5 +23,37 @@ public class CartsController(IMediator mediator, IMapper mapper) : ControllerBas
         var cart = await mediator.Send(query, cancellationToken);
         
         return mapper.Map<GetCartDto>(cart);
+    }
+    
+    [HttpPost("{cartId}/items")]
+    public async Task<ActionStatusDto> AddItemToCart(AddItemDto dto, CancellationToken cancellationToken)
+    {
+        var command = mapper.Map<AddItemToCartCommand>(dto);
+        
+        var success = await mediator.Send(command, cancellationToken);
+
+        return new ActionStatusDto
+        {
+            Status = success,
+            Message = success? "Success": "Failure"
+        };
+    }
+
+    [HttpDelete("{cartId}/items/{itemId}")]
+    public async Task<ActionStatusDto> DeleteItemFromCart(string cartId, Guid itemId, CancellationToken cancellationToken)
+    {
+        var command = new DeleteItemFromCartCommand
+        {
+            CartItemId = itemId,
+            UserId = cartId
+        };
+        
+        var success = await mediator.Send(command, cancellationToken);
+
+        return new ActionStatusDto
+        {
+            Status = success,
+            Message = success? "Success": "Failure"
+        };
     }
 }
