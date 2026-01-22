4e9ca1b185a573d0d9cadfc14cc8b3b1a5af4154
Mike <158204587+Baldj20@users.noreply.github.com>
2026-01-22 16:28:37 +0300

    feat: add unit tests for carts

---
+++ UserService/UserService.UnitTests/Carts/Fakers/Commands/AddItemToCartCommandFaker.cs
@@ -0,0 +1,14 @@
+﻿using Bogus;
+using UserService.BLL.Carts.Commands.AddItemToCart;
+
+namespace UserService.UnitTests.Carts.Fakers.Commands;
+
+public class AddItemToCartCommandFaker : Faker<AddItemToCartCommand>
+{
+    public AddItemToCartCommandFaker()
+    {
+        RuleFor(c => c.UserId, f => f.Lorem.Word());
+        RuleFor(c => c.ProductId, f => f.Random.Guid());
+        RuleFor(c => c.Count, f => f.Random.Int(1, 50));
+    }
+}
---
+++ UserService/UserService.UnitTests/Carts/Tests/Queries/GetUserCartQueryTests.cs
@@ -0,0 +1,23 @@
+﻿using Moq;
+using Shouldly;
+
+namespace UserService.UnitTests.Carts.Tests.Queries;
+
+public class GetUserCartQueryTests : CartMocks
+{
+    [Fact]
+    public async Task GetUserCartQuery_ShouldReturnUserCart()
+    {
+        //Arrange
+        var request = _getUserCartQueryFaker.Generate();
+        var cart = _cartFaker.Generate();
+
+        _cartRepositoryMock.Setup(m => m.GetCartByUserId(request.UserId)).ReturnsAsync(cart);
+        
+        //Act
+        var act = await _getUserCartQueryHandler.Handle(request, CancellationToken.None);
+        
+        //Assert
+        act.ShouldBeEquivalentTo(cart);
+    }
+}
