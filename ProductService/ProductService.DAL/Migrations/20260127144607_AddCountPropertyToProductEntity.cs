77c05997c7b5de9c34ef9a0d6f8757ff6b5acb07
Mike <mihal275@mail.ru>
2026-01-27 17:49:03 +0300

    refactor: add count property add apply migration

---
+++ ProductService/ProductService.DAL/Migrations/20260127144607_AddCountPropertyToProductEntity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductService.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCountPropertyToProductEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Count",
+                table: "Products",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Count",
+                table: "Products");
+        }
+    }
+}
