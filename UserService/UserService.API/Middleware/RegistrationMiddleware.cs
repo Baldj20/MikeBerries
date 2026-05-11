ba7ab75cd2041b3d495c7537a54db633e386e371
Mike <mihal275@mail.ru>
2026-05-11 11:33:16 +0200

    feat(user_service): add registration middleware

---
+++ UserService/UserService.API/Middleware/RegistrationMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using System.Security.Claims;
+using MediatR;
+using Microsoft.Extensions.Caching.Distributed;
+using UserService.BLL.Users.Commands.CreateUser;
+
+namespace UserService.API.Middleware;
+
+public class RegistrationMiddleware(RequestDelegate next, IMediator mediator, IDistributedCache cache)
+{
+    private readonly IDistributedCache _cache = cache;
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        if (context.User.Identity.IsAuthenticated)
+        {
+            var identityId = context.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var email  = context.User.FindFirst(ClaimTypes.Email)?.Value;
+            var name = context.User.FindFirst(ClaimTypes.Name)?.Value;
+            
+            long.TryParse(context.User.FindFirst("iat")?.Value, out long tokenIssuedAt);
+
+            if (!string.IsNullOrEmpty(identityId))
+            {
+                string cacheKey = $"identity_id:{identityId}";
+                
+                var last = await _cache.GetAsync(cacheKey);
+                long lastIssuedAt = last is not null ? BitConverter.ToInt64(last) : 0;
+
+                if (tokenIssuedAt > lastIssuedAt)
+                {
+                    var createUserCommand = new CreateUserCommand
+                    {
+                        IdentityId = identityId,
+                        Name = name,
+                        Email = email,
+                    };
+                    
+                    await mediator.Send(createUserCommand);
+                    
+                    await _cache.SetAsync(cacheKey, BitConverter.GetBytes(tokenIssuedAt),
+                        new DistributedCacheEntryOptions { AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(24) });
+                }
+            }
+        }
+        await next.Invoke(context);
+    }
+}
